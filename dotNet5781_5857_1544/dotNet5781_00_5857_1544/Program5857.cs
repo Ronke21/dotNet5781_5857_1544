5738c8dabaa2e1e63fd23df88e356d9db0689d61
רון קינן <ronke21@gmail.com>
2020-10-20 20:46:49 +0300

    Initial code + Welcome 5857

---
+++ dotNet5781_5857_1544/dotNet5781_00_5857_1544/Program5857.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dotNet5781_00_5857_1544
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5857();
+            Welcome1544();
+            Console.Read();
+        }
+
+        static partial void Welcome1544();
+        private static void Welcome5857()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
