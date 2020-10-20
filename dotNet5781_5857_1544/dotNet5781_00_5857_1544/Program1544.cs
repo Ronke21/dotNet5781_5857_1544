860748ef060f22928ec0193bcfc9ba95f5cb41f9
AmihayHassan <57641622+AmihayHassan@users.noreply.github.com>
2020-10-20 21:01:10 +0300

    Added a message to say I am also here

---
+++ dotNet5781_5857_1544/dotNet5781_00_5857_1544/Program1544.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNet5781_00_5857_1544
+{
+    partial class Program
+    {
+        static partial void Welcome1544()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
