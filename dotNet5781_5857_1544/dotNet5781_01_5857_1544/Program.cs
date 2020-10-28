c9f1d59fa8066295f9524923d3bea0ca39068344
AmihayHassan <57641622+AmihayHassan@users.noreply.github.com>
2020-10-29 00:22:44 +0200

    בעיקר סידור גרפי בנוסף שונו הפונקציות: 1. qualifiedFuel שהיתה בה שגיאה 2. qualifiedDate קוצרה לצורך שיפור יעילות וקריאות

--- dotNet5781_5857_1544/dotNet5781_01_5857_1544/Program.cs
+++ dotNet5781_5857_1544/dotNet5781_01_5857_1544/Program.cs
@@ -8,11 +8,7 @@
 
 
 using System;
-using System.ComponentModel;
 using System.Collections.Generic;
-using System.Xml;
-using System.Threading;
-using System.Runtime.CompilerServices;
 using System.Linq;
 
 namespace dotNet5781_01_5857_1544
@@ -23,73 +19,78 @@ namespace dotNet5781_01_5857_1544
     /// </summary>
     public class Bus
     {
-        private int licenseNum; // licenseNum is the bus id - can't be changed, so property has only get!
+        private int licenseNum;                      // licenseNum is the bus id - can't be changed, so property has only get!
         public int LICENSENUM
         {
             get { return licenseNum; }
         }
-        public DateTime startService { get; set; } //the day that the bus started riding
-        public double fuel { get; set; } //amount of fuel in tank
-        private double mileage; //the total kilometers the bus drived - private so it won't be changed to less, can be only added.
+        public DateTime startService { get; set; }   //the day that the bus started riding
+        public double Fuel { get; set; }             //amount of fuel in tank
+
+        private double mileage;                      //the total kilometers the bus drived - private so it won't be changed to less, can be only added.
         public double MILEAGE
         {
             get { return mileage; }
         }
-        public void addToMileage(int add) //add to milage, public because it is used after rides
+        public void addToMileage(int add)            //add to milage, public because it is used after rides
         {
             if (add > 0)
+            {
                 this.mileage += add;
+            }
         }
-        public double lastMaintMileage { get; set; } = 0; //the Kilometers level in the last maintenance care. for qualifacation
-        public DateTime lastMaintDate { get; set; } //the last date of maintenance care. for qualifacation
+        public double lastMaintMileage { get; set; } = 0;   //the Kilometers level in the last maintenance care. for qualifacation
+        public DateTime lastMaintDate { get; set; }         //the last date of maintenance care. for qualifacation
 
         public Bus(int ID, DateTime begin, int delek, int km, DateTime maint) //constructor, get all the details from user.
         {
             this.licenseNum = ID;
             this.startService = begin;
-            this.fuel = delek;
+            this.Fuel = delek;
             this.mileage = km;
             this.lastMaintDate = maint;
         }
 
-        private bool qualifiedMilage() //checks if we passed 20000 km from last maintenance care -so we can't ride until we do another care
+        //checks if we passed 20,000 km from last maintenance care -so we can't ride until we do another care
+        private bool qualifiedMilage()
         {
             return this.mileage - this.lastMaintMileage <= 20000;
         }
 
-        private bool qualifiedDate() //checks if we passed 1 year from last maintenance care -so we can't ride until we do another care
+        //checks if we passed 1 year from last maintenance care -so we can't ride until we do another one
+        public bool qualifiedDate()
         {
-            DateTime zeroTime = new DateTime(1, 1, 1);
-            TimeSpan t = (DateTime.Now - this.lastMaintDate);
-           int year = (zeroTime + t).Year - 1;
-           return year <= 1;
+            return this.lastMaintDate.AddYears(1).CompareTo(DateTime.Now) > 0;
         }
 
-        private bool qualifiedFuel(int ride) //checks if we passed 1200 km so the fuel tank is empty -so we can't ride until we refuel
+        //checks if we passed 1,200 km which means the fuel tank is empty -so we can't ride until we refuel
+        private bool qualifiedFuel(int ride)
         {
-            return ride + fuel <= 1200;
+            return Fuel - ride >= 0;
         }
 
-        public bool allQuailified(int ride) // a public function that gather all the private qualifaction checks
+        // a public function that gather all the private qualifaction checks
+        public bool allQuailified(int ride)
         {
             return qualifiedFuel(ride) && qualifiedDate() && qualifiedMilage();
         }
 
-        public string stringLicenseNum() // a function that recieve the bus id as an integer and returns as a string in the correct form (xx-xxx-xx)
+        // a function that receive the bus id as an integer and returns as a string in the correct form (xx-xxx-xx)
+        public string stringLicenseNum()
         {
-
             if (licenseNum > 9999999)
             {
                 return this.licenseNum / 100000 + "-" + (this.licenseNum / 1000) % 100 + "-" + this.licenseNum % 1000;
             }
-            else
-            {
-                return this.licenseNum / 100000 + "-" + (this.licenseNum / 100) % 1000 + "-" + this.licenseNum % 100;
-            }
+
+            return this.licenseNum / 100000 + "-" + (this.licenseNum / 100) % 1000 + "-" + this.licenseNum % 100;
+
         }
-        public void printMilageSinceLastMaint() //a func that prints a bus details
+            
+        //a func that prints a bus details
+        public void printMilageSinceLastMaint() 
         {
-            Console.WriteLine("\nBus number: " + this.stringLicenseNum() + "\t\tMileage since last maintenance: " + (this.mileage - this.lastMaintMileage) + "\t\tFuel amount: " + this.fuel + "\t\tMileage: " + this.mileage + "\t\tDate of last maintenance: " + this.lastMaintDate);
+            Console.WriteLine("\nBus number: " + this.stringLicenseNum() + "\t\tMileage since last maintenance: " + (this.mileage - this.lastMaintMileage) + "\t\tFuel amount: " + this.Fuel + "\t\tMileage: " + this.mileage + "\t\tDate of last maintenance: " + this.lastMaintDate);
         }
     }
 
@@ -99,8 +100,10 @@ namespace dotNet5781_01_5857_1544
         {
             List<Bus> DB = new List<Bus>(); // a list of buses - our data base!
             int num;
-            DB.Add(new Bus(1234567, new DateTime(21 / 08 / 1992), 111, 19000, DateTime.Now )); //adding 2 buses to DB in order to check the func
-            DB.Add(new Bus(87654321, new DateTime(21 / 4 / 2019), 888, 10000, DateTime.Now ));
+            
+            //adding 2 buses to DB in order to check the func
+            DB.Add(new Bus(1234567, new DateTime(21 / 08 / 1992), 111, 19000, DateTime.Now.AddYears(-2)));
+            DB.Add(new Bus(87654321, new DateTime(21 / 4 / 2019), 888, 10000, DateTime.Now));
 
             // the main is based on a choosing option that repeat itself and provide the request of the user
             do
@@ -109,7 +112,7 @@ namespace dotNet5781_01_5857_1544
                 Console.WriteLine("1. To enter new bus");
                 Console.WriteLine("2. Choose existing bus");
                 Console.WriteLine("3. Refuel or maintenance update");
-                Console.WriteLine("4. Print milage for all buses");
+                Console.WriteLine("4. Print mileage for all buses");
                 Console.WriteLine("5. Exit");
                 Int32.TryParse(Console.ReadLine(), out num);
                 switch (num)
@@ -124,7 +127,7 @@ namespace dotNet5781_01_5857_1544
                                 Console.WriteLine("Please enter the bus starting date (dd/mm/yyyy): ");
                                 DateTime.TryParse(Console.ReadLine(), out start);
                             } while (start.Year < 1948); //recieve a valid starting date since the establishment of Israel
-                            if (start.Year >2017) //so the ID is 8 digit
+                            if (start.Year > 2017) //so the ID is 8 digit
                             {
                                 do
                                 {
@@ -175,7 +178,7 @@ namespace dotNet5781_01_5857_1544
                                     if (element.allQuailified(rideLength)) //if can make the ride, update the fuel and km
                                     {
                                         Console.WriteLine("The bus made the ride");
-                                        element.fuel += rideLength;
+                                        element.Fuel += rideLength;
                                         element.addToMileage(rideLength);
                                         break;
                                     }
@@ -217,7 +220,7 @@ namespace dotNet5781_01_5857_1544
                             Int32.TryParse(Console.ReadLine(), out int action);
                             if (action == 1) //if want to refuel - fill the tank
                             {
-                                temp.fuel = 1200;
+                                temp.Fuel = 1200;
                                 Console.WriteLine("Fuel updated to 1200!");
                             }
                             else //if want maintenance - update last maint for today
